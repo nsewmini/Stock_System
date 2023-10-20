aa84843329733cdd187e62e6b85ca07c3a454105
Chathurangi  Wijemanna <96134411+Chathurangiw@users.noreply.github.com>
2023-10-20 15:09:52 +0530

    change ui design home

--- STOCK_MANAGEMENT_SYSTEM-01/Homepage.Designer.cs
+++ STOCK_MANAGEMENT_SYSTEM-01/Homepage.Designer.cs
@@ -31,49 +31,83 @@
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(316, 48);
+            button1.BackColor = Color.LightSalmon;
+            button1.Location = new Point(158, 112);
             button1.Name = "button1";
-            button1.Size = new Size(271, 29);
+            button1.Size = new Size(209, 53);
             button1.TabIndex = 0;
             button1.Text = "Go as Admin";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(316, 133);
+            button2.BackColor = Color.LightSalmon;
+            button2.Location = new Point(158, 206);
             button2.Name = "button2";
-            button2.Size = new Size(271, 29);
+            button2.Size = new Size(208, 53);
             button2.TabIndex = 1;
             button2.Text = "AS A SUPPLIER";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(316, 224);
+            button3.BackColor = Color.LightSalmon;
+            button3.Location = new Point(158, 310);
             button3.Name = "button3";
-            button3.Size = new Size(271, 29);
+            button3.Size = new Size(208, 54);
             button3.TabIndex = 2;
             button3.Text = "AS A CUSTOMER";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cambria", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Firebrick;
+            label1.Location = new Point(63, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 40);
+            label1.TabIndex = 3;
+            label1.Text = "HOME";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Screenshot__145_;
+            pictureBox1.Location = new Point(459, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(348, 448);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // Homepage
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Tan;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Controls.Add(pictureBox1);
             Name = "Homepage";
             Text = "Homepage";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -81,5 +115,7 @@
         private Button button1;
         private Button button2;
         private Button button3;
+        private Label label1;
+        private PictureBox pictureBox1;
     }
 }
